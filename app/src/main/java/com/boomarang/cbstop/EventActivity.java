50f31cdac2e1b580c6f42c9c02a7144344366a72
gdyoon <dbsrlehs92@gmail.com>
2017-07-24 22:33:37 +0900

    init application

---
+++ app/src/main/java/com/boomarang/cbstop/EventActivity.java
@@ -0,0 +1,82 @@
+package com.boomarang.cbstop;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.boomarang.cbstop.event.BedFragment;
+import com.boomarang.cbstop.event.CampingFragment;
+import com.boomarang.cbstop.event.EventFragment;
+import com.boomarang.cbstop.event.FoodFragment;
+import com.boomarang.cbstop.event.HistoricFragment;
+import com.boomarang.cbstop.event.TreeFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class EventActivity extends AppCompatActivity {
+
+    @BindView(R.id.vp_event)            ViewPager viewPager;
+    @BindView(R.id.layout_event_tab)    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+        ButterKnife.bind(this);
+
+        int[] arrIconResource = {
+               R.drawable.img_search_historic,
+                R.drawable.img_search_camping,
+                R.drawable.img_search_tree,
+                R.drawable.img_search_fest,
+                R.drawable.img_search_food,
+                R.drawable.img_search_bed
+        };
+
+        Fragment[] arrFragments = new Fragment[6];
+        arrFragments[0] = new HistoricFragment();
+        arrFragments[1] = new CampingFragment();
+        arrFragments[2] = new TreeFragment();
+        arrFragments[3] = new EventFragment();
+        arrFragments[4] = new FoodFragment();
+        arrFragments[5] = new BedFragment();
+
+        EventPagerAdapter adapter = new EventPagerAdapter(getSupportFragmentManager(), arrFragments);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+        for(int i=0; i<tabLayout.getTabCount(); i++) {
+            tabLayout.getTabAt(i).setIcon(arrIconResource[i]);
+        }
+    }
+
+
+
+    class EventPagerAdapter extends FragmentPagerAdapter {
+
+        Fragment[] arrFragments;
+
+        public EventPagerAdapter(FragmentManager fm, Fragment[] arrFragments) {
+            super(fm);
+            this.arrFragments = arrFragments;
+
+
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return arrFragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return arrFragments.length;
+        }
+    }
+}
---
+++ app/src/main/java/com/boomarang/cbstop/SearchActivity.java
@@ -0,0 +1,44 @@
+package com.boomarang.cbstop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @BindView(R.id.sp_search_category)
+    Spinner sp_search_category;
+    private String searchedValue;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+
+        AddItemListToSpinner();
+    }
+
+    private void AddItemListToSpinner()
+    {
+        ArrayAdapter<CharSequence> adapter
+                = ArrayAdapter.createFromResource(this, R.array.category, android.R.layout.simple_spinner_dropdown_item);
+        sp_search_category.setAdapter(adapter);
+
+    }
+
+    @OnClick(R.id.btn_search_search)
+    public void OnSearchButtonClicked()
+    {
+        int position = sp_search_category.getSelectedItemPosition();
+
+        Toast.makeText(getApplicationContext(), ""+position, Toast.LENGTH_SHORT).show();
+    }
+
+}
